27cd414bc54e586172a2f014bd42c003589164e7
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2017-07-19 12:16:38 +0100

    Reformatting the code to make it consistent within the file

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -11,34 +11,34 @@ const async = require('async');
 
 const DocumentPersistence = require('../../../extropy-document-persistence');
 
-var ReceiptRegistry;
-var receiptRegistryInstance;
-var web3;
-var receiptDao;
+let ReceiptRegistry;
+let receiptRegistryInstance;
+let web3;
+let receiptDao;
 
 
 // Private
-var init = function(req, res, next) {
+var init = function (req, res, next) {
     if (web3 === undefined) {
         // set the provider you want from Web3.providers
         web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
     }
 
-    if(receiptRegistryInstance === undefined || ReceiptRegistry === undefined) {
+    if (receiptRegistryInstance === undefined || ReceiptRegistry === undefined) {
         ReceiptRegistry = contract(receiptRegistryAbi);
         ReceiptRegistry.setProvider(web3.currentProvider);
         return ReceiptRegistry.deployed()
-        .then( (instance) => {
-            receiptDao = new ReceiptDao(instance);
-            receiptRegistryInstance = instance;
+            .then((instance) => {
+                receiptDao = new ReceiptDao(instance);
+                receiptRegistryInstance = instance;
 
-            // call next step
-            next();
-        })
-        .catch( (err) => {
-            console.log(err);
-            res.status(500).send('Internal Server Error: ' + err);
-        })
+                // call next step
+                next();
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(500).send('Internal Server Error: ' + err);
+            })
     }
 
     // call next step
@@ -47,8 +47,8 @@ var init = function(req, res, next) {
 
 var retrieveReceiptMetaData = function (metaDataHash) {
     return DocumentPersistence.retrieveDocument(metaDataHash)
-        .then( (metadataData) => {
-            if(metadataData !== undefined && metadataData instanceof Buffer) {
+        .then((metadataData) => {
+            if (metadataData !== undefined && metadataData instanceof Buffer) {
                 return JSON.parse(metadataData.toString());
             }
             return metadataData;
@@ -64,7 +64,7 @@ var retrieveReceiptImageData = function (imageHash) {
             console.log(imageData);
             return DocumentPersistence.retrieveDocument(metaDataHash);
         })
-        .then( (metadataData) => {
+        .then((metadataData) => {
             data.metadata = metadataData;
             console.log("METADATA");
             console.log(metadataData);
@@ -72,28 +72,28 @@ var retrieveReceiptImageData = function (imageHash) {
         });
 };
 
-var getReceiptJson = function(data) {
-    if(data == undefined || ! (data instanceof Map)) {
+var getReceiptJson = function (data) {
+    if (data == undefined || !(data instanceof Map)) {
         return Promise.resolve([]);
     }
 
     let json = [];
     let receipts = Array.from(data.values());
 
-    return new Promise( (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         async.each(
             // Collection
             receipts,
             // Async function
             function (receipt, callback) {
                 retrieveReceiptMetaData(receipt.metadataHash)
-                    .then( (metadata) => {
+                    .then((metadata) => {
                         json.push(
                             {
                                 "id": receipt.receiptId,
                                 "blockNumber": receipt.blockNum,
                                 "image": receipt.imageHash,
-                                "metadata" : metadata
+                                "metadata": metadata
                             }
                         );
                         callback();
@@ -101,7 +101,7 @@ var getReceiptJson = function(data) {
             },
             // callback
             function (err) {
-                if(err) {
+                if (err) {
                     console.log(err);
                     reject(err);
                 }
@@ -112,12 +112,12 @@ var getReceiptJson = function(data) {
     });
 };
 
-var getMetadataJson = function(amount, purchaseType, purchaseDate) {
+var getMetadataJson = function (amount, purchaseType, purchaseDate) {
 
     return {
-        "amount" : amount,
-        "type" : purchaseType,
-        "date" : purchaseDate
+        "amount": amount,
+        "type": purchaseType,
+        "date": purchaseDate
     }
 };
 
@@ -126,20 +126,20 @@ app.use(init);
 app.use(formidable());
 
 app.get('/receipts/:address', function (req, res) {
-  const address = req.params.address;
+    const address = req.params.address;
 
-  return receiptDao.retrieveAllReceipts(address)
-  .then( (receipts) => {
-      console.log(receipts.values());
-      return getReceiptJson(receipts);
-  })
-  .then( (data) => {
-    res.jsonp(data);
-  })
-  .catch( (err) => {
-      console.log(err);
-      res.status(500).send('Internal Server Error' + err);
-  })
+    return receiptDao.retrieveAllReceipts(address)
+        .then((receipts) => {
+            console.log(receipts.values());
+            return getReceiptJson(receipts);
+        })
+        .then((data) => {
+            res.jsonp(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Internal Server Error' + err);
+        })
 });
 
 app.get('/receipt/:id/address/:address', function (req, res) {
@@ -147,17 +147,17 @@ app.get('/receipt/:id/address/:address', function (req, res) {
     const address = req.params.address;
 
     return receiptDao.retrieveReceipt(address, id, 0)
-    .then( (receipt) => {
-        console.log(receipt);
-        return getReceiptJson(receipt);
-    })
-    .then( (data) => {
-        res.jsonp(data);
-    })
-    .catch( (err) => {
-        console.log(err);
-        res.status(500).send('Internal Server Error' + err);
-    })
+        .then((receipt) => {
+            console.log(receipt);
+            return getReceiptJson(receipt);
+        })
+        .then((data) => {
+            res.jsonp(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send('Internal Server Error' + err);
+        })
 
 });
 
@@ -167,23 +167,23 @@ app.get('/receipt/:id/address/:address/image', function (req, res) {
 });
 
 app.post('/receipt', function (req, res) {
-  let json = {};
-  return DocumentPersistence.persistDocument(req.files.fileupload.path)
-  .then( (result) => {
-    json.image = result;
-    const fields = req.fields;
-    const metadataString = JSON.stringify(getMetadataJson(fields.pamount, fields.ptype, fields.pdate));
-    const buffer = Buffer.from(metadataString);
-    return DocumentPersistence.persistDocument(buffer);
-  })
-  .then( (result) => {
-    json.metadata = result;
-    res.jsonp(json);
-  })
-  .catch( (err) => {
-    console.log(err);
-    res.end();
-  })
+    let json = {};
+    return DocumentPersistence.persistDocument(req.files.fileupload.path)
+        .then((result) => {
+            json.image = result;
+            const fields = req.fields;
+            const metadataString = JSON.stringify(getMetadataJson(fields.pamount, fields.ptype, fields.pdate));
+            const buffer = Buffer.from(metadataString);
+            return DocumentPersistence.persistDocument(buffer);
+        })
+        .then((result) => {
+            json.metadata = result;
+            res.jsonp(json);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.end();
+        })
 });
 
 app.listen(3000);
